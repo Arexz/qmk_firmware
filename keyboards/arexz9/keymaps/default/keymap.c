9ae6c5af4ae11ec75506815ea4d558f536c3918f
Arexz <hamsterkatze@gmail.com>
2025-05-12 18:08:13 +0200

    add rules.mk

--- keyboards/arexz9/keymaps/default/keymap.c
+++ keyboards/arexz9/keymaps/default/keymap.c
@@ -1,23 +1,37 @@
+/* Copyright 2020 Nick Brassel (tzarc)
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
 #include QMK_KEYBOARD_H
 
-// Defines names for use in layer keycodes and the keymap
-enum layer_names {
-    _BASE,
-};
-
-const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+//---------//
+//Keymap(s)//
+//---------//
+const uint16_t PROGMEM keymaps[] [MATRIX_ROWS][MATRIX_COLS] = {
     /*
-     * ┌───┬───┬───┐
-     * │ 7 │ 8 │ 9 │
-     * ├───┼───┼───┤
-     * │ 4 │ 5 │ 6 │
-     * ├───┼───┼───┤
-     * │ 1 │ 2 │ 3 │
-     * └───┴───┴───┘
+     * ┌─────┬─────┬─────┐
+     * │ |<< │Pl/Pa│ >>| │
+     * ├─────┼─────┼─────┤
+     * │ F13 │ F14 │ F15 │
+     * ├─────┼─────┼─────┤
+     * │ F16 │ F17 │ F18 │
+     * └─────┴─────┴─────┘
      */
-    [_BASE] = LAYOUT_ortho_3x3(
-        KC_F19,       KC_F20,       KC_F21,
-        KC_F16,       KC_F17,       KC_F18,
-        KC_F13,       KC_F14,       KC_F15
+    [0] = LAYOUT_ortho_3x3(
+        KC_MPRV,    KC_MPLY,    KC_MNXT,
+        KC_F13,    KC_F14,    KC_F15,
+        KC_F16,    KC_F17,    KC_F18
     ),
 };
---
+++ keyboards/arexz9/keymaps/default/rules.mk
@@ -0,0 +1,14 @@
+################
+### rules.mk ###
+################
+
+
+##General Keyboard Setup
+VIA_ENABLE = yes
+LTO_ENABLE = yes
+WPM_ENABLE = no
+RGBLIGHT_ENABLE = no
+SLEEP_LED_ENABLE = no
+
+##HID Communication with Host
+RAW_ENABLE = yes
